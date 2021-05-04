e5447485bb7fdce7f4f38422c8acef9dbd27e198
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-05-04 22:16:00 +0600

    Add solution for problem `A - T or T` Link: https://atcoder.jp/contests/abc133/tasks/abc133_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc133/submissions/22313643

--- atcoder/abc133/A.cpp
+++ atcoder/abc133/A.cpp
@@ -1,60 +1,15 @@
-/********************************************************************************************************************************************************************** 
-                                                                                                                   
-                                                          __Bismillahir Rahmanir Rahim__                                     
-                                                American International University Bangladesh (AIUB)  
-                                                              Hadiur Rahman Nabil                      
-                                                                                                                 
-                                                                                                                  
-***********************************************************************************************************************************************************************/
 #include <bits/stdc++.h>
-
-using namespace std;
-
-typedef long int li;
-typedef long long ll;
-typedef long double ld;
-typedef vector <int> vi;
-
-
-#define pb push_back
-#define mp make_pair
-#define gcd(a,b) __gcd(a,b)
-#define endl "\n"
-
-
-const int mod = 1e5 + 10;
-const double PI = 3.1415926535897932384626;
-const int mod_2 = 998244353;
-const int MAX=100005;
-
-/**********************************************************************************************************************************************************************/
-
-int main()
-{
-    
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    #ifndef ONLINE_JUDGE
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-    #endif
-
-    int n,a,b;
-    cin>>n>>a>>b;
-    if(n*a>b)
-    {
-        cout<<b<<"\n";
-    }
-    else
-    {
-        cout<<n*a<<'\n';
-    }
-
-}
-
-
-
-/*
-*/
+ 
+ using namespace std;
+ 
+ int main() {
+ int N, A, B;
+   
+   cin >> N;
+   cin >> A;
+   cin >> B;
+   
+  int cost=min(N*A,B);
+   cout<< cost <<endl;
+   
+ }
