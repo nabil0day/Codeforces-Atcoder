f197e36360287372716dfa2d16c7cb6cab37c1cf
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-05-17 22:01:00 +0600

    Add solution for problem `A - AtCoder Crackers` Link: https://atcoder.jp/contests/abc105/tasks/abc105_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc105/submissions/22696821

---
+++ atcoder/abc105/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long N,K;
+    cin>> N>>K;
+    if( N%K==0) 
+    {
+        cout<< 0;
+    }
+    else
+    { 
+        cout<< 1;
+    }
+    return 0;
+
+}
