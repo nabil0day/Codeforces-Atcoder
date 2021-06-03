36e5b2f85d876f68a09f40914b6d06c17a68843d
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-06-03 11:55:00 +0600

    Add solution for problem `A - Elephant` Link: https://codeforces.com/contest/617/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/617/submission/118245936

---
+++ codeforces/617/A.cpp
@@ -0,0 +1,31 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+ 
+
+ 
+int main()
+{
+
+  int x;
+  cin>>x;
+  if(x%5==0)
+  {
+    cout<<x/5;
+  }
+else 
+{
+  cout<< (x/5)+1;
+}
+return 0;
+}
+ 
