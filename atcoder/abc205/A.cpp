e938f9701b822fba893a226293c99be706ddb339
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-06-13 18:15:00 +0600

    Add solution for problem `A - kcal` Link: https://atcoder.jp/contests/abc205/tasks/abc205_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc205/submissions/23418521

---
+++ atcoder/abc205/A.cpp
@@ -0,0 +1,30 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ 
+
+
+
+int main()
+{
+    
+    int a,b;
+    float c,d;
+    cin>>a>>b;
+    c=(a*b);
+    d=c/100;
+    cout<<d<<endl;
+  
+return 0;
+
+}
+ 
