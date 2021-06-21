7799825d2df3108d38834bd9b10c28a952a08843
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-06-21 20:36:00 +0600

    Add solution for problem `A - Translation` Link: https://codeforces.com/contest/41/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/41/submission/120217731

---
+++ codeforces/41/A.cpp
@@ -0,0 +1,35 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+
+string s;
+string t;
+cin>>s;
+cin>>t;
+reverse(s.begin(),s.end());
+
+if(s==t)
+{
+  cout<<"YES\n";
+}
+else
+{
+    cout<<"NO\n";
+}
+return 0;
+
+
+
+}
+ 
