2ceef2d670e8b2aefd2c367eb39f828411014f81
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-06-23 22:06:00 +0600

    Add solution for problem `A - George and Accommodation` Link: https://codeforces.com/contest/467/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/467/submission/120382647

---
+++ codeforces/467/A.cpp
@@ -0,0 +1,33 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    int n,x,y,count=0;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+       
+        cin>>x>>y;
+        if(y-x>1)
+        {
+            count++;
+        }
+         
+    }
+    
+   cout<<count<<endl;
+  
+return 0;
+
+}
+ 
