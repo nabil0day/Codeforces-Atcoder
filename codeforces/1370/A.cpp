b25c4e0eb8d0bf372cc57697b4d89e93f0dccf16
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-06-26 19:58:00 +0600

    Add solution for problem `A - Maximum GCD` Link: https://codeforces.com/contest/1370/problem/A Tags: greedy, implementation, math, number theory, *800 Ref: https://codeforces.com/contest/1370/submission/120675499

---
+++ codeforces/1370/A.cpp
@@ -0,0 +1,29 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+  
+int main()
+{
+  ll a,b;
+  cin>>a;
+  for(int i=1;i<=a;i++)
+  {
+     cin>>b;
+     cout<<b/2<<endl;
+
+  }
+}
+
+ 
+
+ 
+
+
