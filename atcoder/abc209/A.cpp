d41bb6d269de198d6aec851d3c213fdb912c4abb
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-10 18:32:00 +0600

    Add solution for problem `A - Counting` Link: https://atcoder.jp/contests/abc209/tasks/abc209_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc209/submissions/24128601

---
+++ atcoder/abc209/A.cpp
@@ -0,0 +1,67 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+typedef long double ld;
+
+//Palindrome--
+// for(int i=s.size()-1;i>=0;--i){s_rev.push_back(s[i]);} if(s==s_rev)    
+ //Anagram--
+ //sort(a.begin(), a.end());sort(b.begin(), b.end());if(a==b)
+  
+//Find_Last + Delete_Last--
+//num%10    +   num/10
+//Fibonacci--
+//for(int i=1;i<=N-2;i++){ num3=num1(1)+num2(2);num1=num2;num2=num3(Fibonacci);
+
+
+
+
+
+  
+
+
+    int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+ 
+   ll a,b,num=0;
+   cin>>a>>b;
+   
+    for(int i=a;i<=b;i++)
+    {
+        num++;
+    }
+    cout<<num<<"\n";
+    
+ return 0;
+}
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
