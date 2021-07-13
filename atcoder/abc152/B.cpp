5793648007112b1da767e7e6324c6882f1771956
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-13 23:17:00 +0600

    Add solution for problem `B - Comparing Strings` Link: https://atcoder.jp/contests/abc152/tasks/abc152_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc152/submissions/24210056

---
+++ atcoder/abc152/B.cpp
@@ -0,0 +1,71 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+typedef long double ld;
+
+//Palindrome--
+// for(int i=s.size()-1;i>=0;--i){s_rev.push_back(s[i]);} if(s==s_rev)    
+
+ //sort(a.begin(), a.end());sort(b.begin(), b.end());if(a==b)
+  
+//Find_Last + Delete_Last--
+//num%10    +   num/10
+//Fibonacci--
+//for(int i=1;i<=N-2;i++){ num3=num1(1)+num2(2);num1=num2;num2=num3(Fibonacci);
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int a, b;
+	cin >> a >> b;
+	for(int i = 0;i < max(a, b);i++){
+		cout << min(a, b);
+	}
+	cout << endl;
+
+    
+    
+   
+    
+        return 0;
+}
+
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
