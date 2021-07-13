86ca5260dbddeb1fbd70afabcf4e3e62e548b176
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-13 23:53:00 +0600

    Add solution for problem `A - Repeating Cipher` Link: https://codeforces.com/contest/1095/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1095/submission/122372343

---
+++ codeforces/1095/A.cpp
@@ -0,0 +1,78 @@
+/*****                                            
+ *                                                   __Bismillahir Rahmanir Rahim__
+ *                                          American International University Bangladesh (AIUB) 
+ *                                                         Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n'
+
+typedef long long ll;
+typedef long double ld;
+
+//Palindrome--
+// for(int i=s.size()-1;i>=0;--i){s_rev.push_back(s[i]);} if(s==s_rev)    
+
+ //sort(a.begin(), a.end());sort(b.begin(), b.end());if(a==b)
+  
+//Find_Last + Delete_Last--
+//num%10    +   num/10
+//Fibonacci--
+//for(int i=1;i<=N-2;i++){ num3=num1(1)+num2(2);num1=num2;num2=num3(Fibonacci);
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int num=1;
+    for(int i=0;i<n;i++)
+    {
+        
+       
+        cout<<s[i];
+        i+=num;
+        num++;
+        
+
+    }
+    cout<<"\n";
+   
+    
+        return 0;
+}
+
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
