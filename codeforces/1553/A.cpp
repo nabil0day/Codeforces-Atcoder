b0b641b05c0bf4271578ff46c113dc62826fa2df
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-22 21:00:00 +0600

    Add solution for problem `A - Digits Sum` Link: https://codeforces.com/contest/1553/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1553/submission/123304723

---
+++ codeforces/1553/A.cpp
@@ -0,0 +1,66 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair <int,int> pi;
+
+#define endl "\n"
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+ll a,t;
+cin>>t;
+while(t--)
+{
+cin>>a;
+cout<<(a+1)/10<<"\n";
+
+}
+
+
+return 0;
+}
+
+
+        
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
