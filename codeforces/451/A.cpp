e38558fa64d260b5b1e5dc9d2e45242e4379afd2
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-25 00:18:00 +0600

    Add solution for problem `A - Game With Sticks` Link: https://codeforces.com/contest/451/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/451/submission/123639256

---
+++ codeforces/451/A.cpp
@@ -0,0 +1,67 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+
+#define INF 1000000007
+
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+
+
+
+  
+int main()
+{
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+ll n,m;
+    cin>>n>>m;
+ 
+    ll g = min(n,m);
+    if(g%2==0){
+        cout<<"Malvika"<<endl;
+    }
+    else{
+        cout<<"Akshat"<<endl;
+    }
+
+}
+
+        
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
