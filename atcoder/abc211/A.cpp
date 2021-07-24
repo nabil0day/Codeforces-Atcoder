00bdb7953bb795930ba63f8d9624d681a7661336
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-24 18:09:00 +0600

    Add solution for problem `A - Blood Pressure` Link: https://atcoder.jp/contests/abc211/tasks/abc211_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc211/submissions/24492068

---
+++ atcoder/abc211/A.cpp
@@ -0,0 +1,60 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+
+#define INF 1000000007
+
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+
+  
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    float a,b;
+    float c,d;
+    cin>>a>>b;
+    d=(a-b)/3.00;
+    c=d+b;
+    cout<<c<<"\n";
+   return 0;
+}
+
+        
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
