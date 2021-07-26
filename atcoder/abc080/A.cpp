6db93ee964355c38c126128427119b13640b8e53
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-07-26 15:02:00 +0600

    Add solution for problem `A - Parking` Link: https://atcoder.jp/contests/abc080/tasks/abc080_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc080/submissions/24561481

---
+++ atcoder/abc080/A.cpp
@@ -0,0 +1,75 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector <int> vi;
+
+#define INF 1000000007
+
+#define pb push_back
+#define mp make_pair
+#define endl "\n"
+
+
+
+  
+int main()
+{
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+
+
+ll a,b,c,x,y;
+cin>>a>>b>>c;
+x= a*b;
+y= min(x,c);
+cout<< y << "\n";
+return 0;
+
+
+}
+    
+
+    
+
+
+
+
+
+
+        
+
+        
+
+    
+ 
+
+    
+
+    
+
+  
+    
+    
+    
+
+
+
+
+
+ 
+
+ 
+
+
