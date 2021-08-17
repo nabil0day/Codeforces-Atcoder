35eb6f53a0c351ee1590b2706d820fd1087a9c48
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-08-17 10:14:00 +0600

    Add solution for problem `A - Marketing Scheme` Link: https://codeforces.com/contest/1437/problem/A Tags: brute force, constructive algorithms, greedy, math, *800 Ref: https://codeforces.com/contest/1437/submission/126140315

---
+++ codeforces/1437/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int l,r;
+        cin>>l>>r;
+        if(2*l>r) cout<<"YES"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
