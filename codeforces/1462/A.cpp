5c1e5b0a04d412f75f8f6219deaa3fb3e9bdb6fc
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-08-27 00:37:00 +0600

    Add solution for problem `A - Favorite Sequence` Link: https://codeforces.com/contest/1462/problem/A Tags: implementation, two pointers, *800 Ref: https://codeforces.com/contest/1462/submission/127134455

---
+++ codeforces/1462/A.cpp
@@ -0,0 +1,71 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+
+void solve()
+{
+        ll a;
+        cin>>a;
+        ll arr[a];
+        ll brr[a];
+        for(int i=0;i<a;i++) cin>>arr[i];
+        ll i=0,j=a-1;
+        ll k=0;
+        while(i<=j)
+        {
+            brr[k]=arr[i];i++;
+            k++;
+            brr[k]=arr[j];j--;
+            k++;
+        }
+        for(int i=0;i<a;i++) cout<<brr[i]<<" ";
+        cout<<"\n";
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+ ll t;
+ cin>>t;
+ while(t--)
+ {
+     solve();
+ }
+    
+  return 0;
+}
