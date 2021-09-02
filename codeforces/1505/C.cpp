b0a4f8f3a2c278db9cfe11789049c1f7bbdb7656
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-03 00:41:00 +0600

    Add solution for problem `C - Fibonacci Words` Link: https://codeforces.com/contest/1505/problem/C Tags: *special problem, implementation, *1400 Ref: https://codeforces.com/contest/1505/submission/127719058

---
+++ codeforces/1505/C.cpp
@@ -0,0 +1,51 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+
+string s;
+ cin>>s;
+ for(int i=2;i<s.size();i++)
+ {
+      if (s[i]-'A' == (s[i-1]-'A'+s[i-2]-'A')%26)
+            continue;
+        cout<<"NO"<<endl;
+        return 0;
+ }
+ cout<<"YES\n";
+
+}
