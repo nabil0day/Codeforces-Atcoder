6222f31f0662e80e349d4bda9e148fced76df1fc
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-04 21:52:00 +0600

    Add solution for problem `B - AtCoder Quiz` Link: https://atcoder.jp/contests/abc217/tasks/abc217_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc217/submissions/25615817

---
+++ atcoder/abc217/B.cpp
@@ -0,0 +1,51 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+
+set<string> S={"ABC","ARC","AGC","AHC"};
+for(int i=0;i<4;i++)
+{
+    string x;
+    cin>>x;
+    S.erase(x);
+
+}
+cout<<(*S.begin())<<"\n";
+ return 0;
+ 
+}
