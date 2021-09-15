3bd70b42b57173df303008582a15444dcc87b9d9
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-15 23:35:00 +0600

    Add solution for problem `A - Games` Link: https://codeforces.com/contest/268/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/268/submission/128927758

---
+++ codeforces/268/A.cpp
@@ -0,0 +1,74 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+ll n;
+cin>>n;
+ll a[n],b[n],sol=0;
+for(int i=0;i<n;i++)
+{
+    cin>>a[i];
+    cin>>b[i];
+}
+for(int i=0;i<n;i++)
+{
+    for(int j=0;j<n;j++)
+    {
+        if(i==j)
+        {
+            // kichu korar nai
+        }
+        else
+        {
+            if(a[i]==b[j])
+            {
+                sol++;
+            }
+        }
+    }
+}
+cout<<sol<<"\n";
+
+   return 0;
+ 
+}
+
+
+/*
+
+*/
