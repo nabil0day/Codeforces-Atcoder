e707a87829911f810fb25d09e35a762ae686a4d8
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-15 11:03:00 +0600

    Add solution for problem `A - Arpa’s hard exam and Mehrdad’s naive cheat` Link: https://codeforces.com/contest/742/problem/A Tags: implementation, math, number theory, *1000 Ref: https://codeforces.com/contest/742/submission/128869822

---
+++ codeforces/742/A.cpp
@@ -0,0 +1,61 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+ll n;
+cin>>n;
+if(n==0)
+  cout<<1;
+else
+{
+  switch(n%4)
+  {
+      case 0: cout<<6; break;
+      case 1: cout<<8; break;
+      case 2: cout<<4; break;
+      case 3: cout<<2; break;
+  }
+}
+   return 0;
+ 
+}
+
+
+/*
+
+*/
