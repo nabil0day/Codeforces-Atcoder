c73318789988b4c3431b733c421312d3bbefb6c3
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-21 20:57:00 +0600

    Add solution for problem `A - Dense Array` Link: https://codeforces.com/contest/1490/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1490/submission/129507243

---
+++ codeforces/1490/A.cpp
@@ -0,0 +1,79 @@
+/********************************************************************************************************************************************************************** 
+                                                                                                                   
+                                                          __Bismillahir Rahmanir Rahim__                                     
+                                                American International University Bangladesh (AIUB)  
+                                                              Hadiur Rahman Nabil                      
+                                                                                                                 
+                                                                                                                  
+***********************************************************************************************************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+void solve()
+{
+ll n;
+cin>>n;
+ll a[n];
+
+for(int i=0;i<n;i++)
+{
+  cin>>a[i];
+}
+ll count=0;
+for(int i=0;i<n-1;i++)
+{
+  ll ma=max(a[i],a[i+1]);
+  ll mi=min(a[i],a[i+1]);
+  while(ma>2*mi)
+  {
+    mi=mi*2;
+    count++;
+  }
+}
+cout<<count<<"\n";
+
+
+}
+    
+
+
+   
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    
+    
+
+ ll t;
+ cin>>t;
+ while(t--)
+ {
+     solve();
+ }
+    
+  return 0;
+}
