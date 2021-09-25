15ce82933ad2f62ccd4aa2ffee323d2a43140ac4
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-25 15:11:00 +0600

    Add solution for problem `A - Gennady and a Card Game` Link: https://codeforces.com/contest/1097/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/1097/submission/129833944

---
+++ codeforces/1097/A.cpp
@@ -0,0 +1,66 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+
+string s,str;
+cin>>s;
+bool ok=false;
+for(int i=0;i<5;i++)
+{
+  cin>>str;
+  if(s[0]==str[0] || s[1]==str[1])
+  ok=true;
+}
+if(ok)
+{
+  cout<<"YES\n";
+}
+else{
+  cout<<"NO\n";
+}
+
+
+
+  return 0;
+}
+
+
+/*
+
+*/
