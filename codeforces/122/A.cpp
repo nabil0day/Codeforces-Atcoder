172cc0402fa2bf521cb53aed83eece635f348ac3
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-09-28 01:14:00 +0600

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/130035533

---
+++ codeforces/122/A.cpp
@@ -0,0 +1,53 @@
+/*****                                            
+                                                     __Bismillahir Rahmanir Rahim__
+                                               American International University Bangladesh (AIUB) 
+                                                          Hadiur Rahman Nabil
+                                                                 
+                                                               
+                                                          
+*****/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+const int mod = 1e9 + 7;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+ int n,a[]={4,7,44,47,74,77,444,447,474,477,744,747,774,777};
+    cin>>n;
+    for(int i=0;i<14;i++) if(n%a[i]==0)
+    {
+       cout<<"YES\n";
+       return 0;
+    }
+    cout<<"NO\n";
+  return 0;
+}
+
+
+/*
+
+*/
