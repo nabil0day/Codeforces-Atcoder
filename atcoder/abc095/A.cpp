a6bdda3ee0260213aef16f4c7bb15c83756a7b8c
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-10-21 14:18:00 +0600

    Add solution for problem `A - Something on It` Link: https://atcoder.jp/contests/abc095/tasks/abc095_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc095/submissions/26715500

---
+++ atcoder/abc095/A.cpp
@@ -0,0 +1,62 @@
+/********************************************************************************************************************************************************************** 
+                                                                                                                   
+                                                          __Bismillahir Rahmanir Rahim__                                     
+                                                American International University Bangladesh (AIUB)  
+                                                              Hadiur Rahman Nabil                      
+                                                                                                                 
+                                                                                                                  
+***********************************************************************************************************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long int li;
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+
+const int mod = 1e5 + 10;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+/**********************************************************************************************************************************************************************/
+
+int main()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+string s;
+cin>>s;
+int sol=700;
+if(s[0]=='o')
+sol=sol+100;
+if(s[1]=='o')
+sol=sol+100;
+if(s[2]=='o')
+sol=sol+100;
+
+cout<<sol<<"\n";
+
+}
+
+
+
+/*
+*/
+
