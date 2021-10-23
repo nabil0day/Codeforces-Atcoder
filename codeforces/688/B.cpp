9fffd52c55ca66f5abb380166e85bcd95997ae38
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-10-23 19:36:00 +0600

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/132787223

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define int long long 
+#define endl "\n"
+
+int32_t main(){
+    Fast;
+    int t=1;
+    // cin>>t;
+    while(t--){
+       string n ;
+       cin>>n;
+       cout<<n;
+
+      reverse(n.begin() , n.end());
+      cout<<n;
+       
+
+}
+
+ 
+    return 0;
+}
