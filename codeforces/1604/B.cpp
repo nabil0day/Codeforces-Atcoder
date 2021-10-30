54008733beda7ef0601be9c837a7f30ee26ee1f7
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2021-10-30 21:44:00 +0600

    Add solution for problem `B - XOR Specia-LIS-t` Link: https://codeforces.com/contest/1604/problem/B Tags: *1100 Ref: https://codeforces.com/contest/1604/submission/133662410

---
+++ codeforces/1604/B.cpp
@@ -0,0 +1,104 @@
+/********************************************************************************************************************************************************************** 
+                                                                                                                   
+                                                          __Bismillahir Rahmanir Rahim__                                     
+                                                American International University Bangladesh (AIUB)  
+                                                              Hadiur Rahman Nabil                      
+                                                                                                                 
+                                                                                                                  
+***********************************************************************************************************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long int li;
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define gcd(a,b) __gcd(a,b)
+#define endl "\n"
+
+
+const int mod = 1e5 + 10;
+const double PI = 3.1415926535897932384626;
+const int mod_2 = 998244353;
+const int MAX=100005;
+
+
+/**********************************************************************************************************************************************************************/
+//Palindrome--
+// for(int i=s.size()-1;i>=0;--i){s_rev.push_back(s[i]);} if(s==s_rev)    
+ 
+//sort
+//(a.begin(), a.end());sort(b.begin(), b.end());if(a==b)
+  
+//Find_Last + Delete_Last--
+//num%10    +   num/10
+
+//Fibonacci--
+//for(int i=1;i<=N-2;i++){ num3=num1(1)+num2(2);num1=num2;num2=num3(Fibonacci);
+
+
+void solve()
+{
+  ll n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++)
+  cin>>a[i];
+  if(n%2==0)
+  cout<<"YES\n";
+  else
+  {
+    bool ok =false;
+    for(int i=0;i<n-1;i++)
+    if(a[i]>=a[i+1])
+    {
+      ok=true;
+      break;
+    }
+    if(ok)
+    cout<<"YES\n";
+    else{
+      cout<<"NO\n";
+    }
+
+    
+  }
+
+
+  
+
+}
+
+
+    
+
+
+   
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+    
+    
+
+ ll t;
+ cin>>t;
+ while(t--)
+ {
+     solve();
+ }
+    
+  return 0;
+}
