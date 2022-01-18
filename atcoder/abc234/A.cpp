def1658b2f990ed3696cfa39ec2252d588765ec5
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2022-01-18 12:57:00 +0600

    Add solution for problem `A - Weird Function` Link: https://atcoder.jp/contests/abc234/tasks/abc234_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc234/submissions/28614898

---
+++ atcoder/abc234/A.cpp
@@ -0,0 +1,60 @@
+/********************************************************************************************************************************************************************** 
+                                                                                                                   
+                                                          __Bismillahir Rahmanir Rahim__                                     
+                                                American International University Bangladesh (AIUB)  
+                                                              Hadiur Rahman Nabil                      
+                                                                                                                 
+                                                                                                                  
+***********************************************************************************************************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long int      li;
+typedef long long     ll;
+typedef long double   ld;
+typedef vector <int>  vi;
+typedef vector<ll>    vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll>   pll;
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb            push_back
+#define mp            make_pair
+#define gcd(a,b)      __gcd(a,b)
+#define FOR(i,n)      for(int i=0;i<n;i++)
+#define rep(i,k,n)    for(int i=k; i<n; i++)
+#define repp(i,k,n)   for(int i=k; i<=n; i++)6
+#define endl          "\n"
+
+
+const int             mod=1e5 + 10;
+const double          PI=3.1415926535897932384626;
+const int             mod_2 = 998244353;
+const int             MAX=100005;
+const double          PII=2.0*acos(0.0);
+
+
+/************************************************************************************************************************************************/
+int f(int x)
+{
+    return (x*x)+2*x+3;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    cin>>t;
+    cout<<f(f(f(t)+t)+f(f(t)))<<"\n";
+
+
+   return 0;
+
+}
