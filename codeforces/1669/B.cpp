790981b08d64549339759b245f3f4d11ce3b2f83
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2022-04-28 12:36:00 +0600

    Add solution for problem `B - Triple` Link: https://codeforces.com/contest/1669/problem/B Tags: implementation, sortings, *800 Ref: https://codeforces.com/contest/1669/submission/155179936

---
+++ codeforces/1669/B.cpp
@@ -0,0 +1,77 @@
+/**************************************
+                                      *
+ * Author: Hadiur Rahman Nabil        *
+                                      *
+**************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long int      li;
+typedef long long     ll;
+typedef long double   ld;
+typedef vector <int>  vi;
+typedef vector<ll>    vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll>   pll;
+
+#define FAST          ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define pb            push_back
+#define mp            make_pair
+#define gcd(a,b)      __gcd(a,b)
+#define FOR(i,n)      for(int i=0;i<n;i++)
+#define rep(i,k,n)    for(int i=k; i<n; i++)
+#define repp(i,k,n)   for(int i=k; i<=n; i++)6
+#define endl          "\n"
+
+
+const int             mod=1e5 + 10;
+const double          PI=3.1415926535897932384626;
+const int             mod_2 = 998244353;
+const int             MAX=100005;
+const double          PII=2.0*acos(0.0);
+
+
+/**********************************************************************************************************************************************************************/
+
+void solve()
+{
+  ll n;
+  ll ans= -1;
+  cin>>n;
+  vector<ll>v(n);
+  for(int i=0;i<n;i++)
+    cin>>v[i];
+    sort(v.begin(),v.end());
+    for(int i=0;i<n;i++)
+    {
+    if(v[i]==v[i+1] and v[i]==v[i+2])
+    {
+      ans= v[i];
+      break;
+    }
+  }
+  cout<<ans<<"\n";
+}
+
+
+
+int main()
+{
+    FAST;
+
+    #ifndef ONLINE_JUDGE
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    #endif
+
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+      solve();
+     
+    } 
+   
+  return 0;
+}
